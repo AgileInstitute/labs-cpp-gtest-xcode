c20c5ad7c1da2e9069499c586693f1e64a71b7b5
Rob Myers <Rob.Myers@agileInstitute.com>
2014-11-29 17:19:18 -0800

    Getting TestedTrek project prepped for real code

---
+++ TestedTrek/build/Example.build/Debug/UnitTest.build/Objects-normal/x86_64/Example_Tests.d
@@ -0,0 +1,20 @@
+dependencies: /Users/mfuerch/Downloads/Example/tests/Example_Tests.cpp \
+  /Users/mfuerch/Downloads/Example/gtest/gtest.framework/Headers/gtest.h \
+  /Users/mfuerch/Downloads/Example/gtest/gtest.framework/Headers/internal/gtest-internal.h \
+  /Users/mfuerch/Downloads/Example/gtest/gtest.framework/Headers/internal/gtest-port.h \
+  /Users/mfuerch/Downloads/Example/gtest/gtest.framework/Headers/gtest-message.h \
+  /Users/mfuerch/Downloads/Example/gtest/gtest.framework/Headers/internal/gtest-string.h \
+  /Users/mfuerch/Downloads/Example/gtest/gtest.framework/Headers/internal/gtest-filepath.h \
+  /Users/mfuerch/Downloads/Example/gtest/gtest.framework/Headers/internal/gtest-type-util.h \
+  /Users/mfuerch/Downloads/Example/gtest/gtest.framework/Headers/gtest-death-test.h \
+  /Users/mfuerch/Downloads/Example/gtest/gtest.framework/Headers/internal/gtest-death-test-internal.h \
+  /Users/mfuerch/Downloads/Example/gtest/gtest.framework/Headers/gtest-param-test.h \
+  /Users/mfuerch/Downloads/Example/gtest/gtest.framework/Headers/internal/gtest-param-util.h \
+  /Users/mfuerch/Downloads/Example/gtest/gtest.framework/Headers/internal/gtest-linked_ptr.h \
+  /Users/mfuerch/Downloads/Example/gtest/gtest.framework/Headers/gtest-printers.h \
+  /Users/mfuerch/Downloads/Example/gtest/gtest.framework/Headers/internal/gtest-param-util-generated.h \
+  /Users/mfuerch/Downloads/Example/gtest/gtest.framework/Headers/gtest_prod.h \
+  /Users/mfuerch/Downloads/Example/gtest/gtest.framework/Headers/gtest-test-part.h \
+  /Users/mfuerch/Downloads/Example/gtest/gtest.framework/Headers/gtest-typed-test.h \
+  /Users/mfuerch/Downloads/Example/gtest/gtest.framework/Headers/gtest_pred_impl.h \
+  /Users/mfuerch/Downloads/Example/src/Example.h
---
+++ TestedTrek/src/Example.h
@@ -0,0 +1,17 @@
+//
+//  Example.h
+//  Example
+//
+
+#pragma once
+
+class ExampleClass {
+public:
+	ExampleClass();
+	
+	int GetValue();
+	void DoSomething();
+	
+private:
+	int m_iValue;
+};
