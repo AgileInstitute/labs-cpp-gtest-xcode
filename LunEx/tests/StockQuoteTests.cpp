fb56f7acf6a8a55562ee4ff801581ff52b0f9a66
Rob Myers <Rob.Myers@agileInstitute.com>
2014-12-07 19:33:52 -0800

    GMock for LunEx...sort of
    Unzipped, built, and now cannot find the .framework file.

--- LunEx/tests/StockQuoteTests.cpp
+++ LunEx/tests/StockQuoteTests.cpp
@@ -1,4 +1,12 @@
+// required includes
+
 #include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+// some mock behaviors
+
+using ::testing::Return;
+using ::testing::Throw;
 
 TEST(StockQuote, Total) {
     // TODO: Write a test here
